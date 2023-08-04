32f4ac3b03f3fa4a8384b2419bf17c736a8b0c63
Rabaka07 <rabaquimruan07@gmail.com>
2023-08-04 16:57:17 -0300

    oioi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var toggleMade = () =>{
-    const html=document.documentElement;
-    html.classList.toggle("light");
+var toggleMade = () => {
+  const html = document.documentElement;
+  html.classList.toggle("light");
 };
