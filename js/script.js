186cc2a41a53f1ec64ee858fe5b8cc00e5ed5942
Rabaka07 <rabaquimruan07@gmail.com>
2023-08-10 09:44:22 -0300

    ajuste pg mobile, troca de img e acessibilidade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-var toggleMade = () => {
+var toggleMode = () => {
   const html = document.documentElement;
   html.classList.toggle("light");
+  const isLinght = html.classList.contains("light");
+  const srcValue= isLinght? "../img/blanco.jpg" : "../img/avatar.jpg";
+  const altValue= isLinght?"imagem de perfil com avatar na cor branca-Modo claro" : "imagem de perfil com avatar na cor preta-Modo escuro"
+
+  document.querySelector("#profile img").setAttribute("src",srcValue)
+  document.querySelector("#profile img").setAttribute("alt", altValue)
 };
